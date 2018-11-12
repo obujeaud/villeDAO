7442557ae1a55f7a6883983630186f82af95bb9f
granarolo sébastien <granarolo.sebastien@gmail.com>
2018-11-12 15:13:31 +0100

    change name of package presistence to persistance

--- src/persistance/dao/VilleDAO.java
+++ src/persistance/dao/VilleDAO.java
@@ -1,4 +1,4 @@
-package persistence.dao;
+package persistance.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-import persistence.manager.JDBCManager;
+import persistance.manager.JDBCManager;
 
 public class VilleDAO {
 
--- src/persistance/manager/JDBCManager.java
+++ src/persistance/manager/JDBCManager.java
@@ -1,4 +1,4 @@
-package persistence.manager;
+package persistance.manager;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
