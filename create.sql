0af57b80868ef1802d65b2d6e586b447384ab24c
Olivier Bujeaud <35693665+obujeaud@users.noreply.github.com>
2018-11-12 14:34:21 +0100

    Creation projet villedao

---
+++ create.sql
@@ -0,0 +1,34 @@
+-- --------------------------------------------------------
+-- Hôte :                        127.0.0.1
+-- Version du serveur:           5.5.61 - MySQL Community Server (GPL)
+-- SE du serveur:                Win64
+-- HeidiSQL Version:             9.5.0.5196
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Export de la structure de la base pour git
+CREATE DATABASE IF NOT EXISTS `git` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `git`;
+
+-- Export de la structure de la table git. ville
+CREATE TABLE IF NOT EXISTS `ville` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_ville` int(11) DEFAULT NULL,
+  `pays` int(11) DEFAULT NULL,
+  `nbHabitant` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Export de données de la table git.ville : ~0 rows (environ)
+/*!40000 ALTER TABLE `ville` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ville` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ drop.sql
@@ -0,0 +1,1 @@
+DROP DATABASE IF EXISTS `voiture_test`;
